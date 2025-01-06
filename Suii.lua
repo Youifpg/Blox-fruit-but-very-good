5123b02f1216c9cc2522ec187aabc7ef5b23ff62
ARBIX <youssefalsadick@gmail.com>
2025-01-06 16:27:30 +0300

    Update Suii.lua

--- Suii.lua
+++ Suii.lua
@@ -23,8 +23,7 @@ Section:CreateButton("Damage all players", function()
                     Remote:FireServer("EMMFOSS__!ZCNSJNXCSDWQSANBX","Main_DamgeR___",{character, {Using = "Combat", Damge = 9999, FromPlayer = localPlayer}})
                 end
             end
-        end
-end)
+        end)
 
 Section:CreateButton("Fling everything", function()
     for _, character in pairs(npc:GetChildren()) do
@@ -35,30 +34,13 @@ Section:CreateButton("Fling everything", function()
 end)
 
 local Section = Window:NewSection("FRUITS")
-
--- Button to get Dragon West fruit
-Section:CreateButton("Get Dragon West Fruit", function()
+local fruits = {"Dragon (West) Fruit", "Dragon (East) Fruit", "OrbCandy", "OrbHalloween", "OrbQuake", "OrbShanks", "Orbblack", "Orbblue", "Orbcures", "Orbflame", "Orbgreen", "Orbkj", "Orbshadow", "Orbsky", "finger"}
+Section:CreateButton("Get all fruits", function()
     local args = {
         [1] = "EMMFOSS__!ZCNSJNXCSDWQSANBX",
         [2] = "AddToolToBackpackKKK",
         [3] = {
             [1] = "Dragon (West) Fruit",
-            [2] = localPlayer.Backpack,
-            [3] = true,
-            [4] = true
-        }
-    }
-
-    Remote:FireServer(unpack(args))
-end)
-
--- Button to get Dragon East fruit
-Section:CreateButton("Get Dragon East Fruit", function()
-    local args = {
-        [1] = "EMMFOSS__!ZCNSJNXCSDWQSANBX",
-        [2] = "AddToolToBackpackKKK",
-        [3] = {
-            [1] = "Dragon (East) Fruit",
             [2] = localPlayer.Backpack,
             [3] = true,
             [4] = true
